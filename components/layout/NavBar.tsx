632869cb36b74d34f9d406c268ca04cfe05e08ed
Francisco Gustavo Mendez <franciscomendez20@hotmail.com>
2025-07-26 15:39:13 -0300

    Nav Almost Completed

--- components/layout/NavBar.tsx
+++ components/layout/NavBar.tsx
@@ -1,21 +1,24 @@
 import Container from "./Container";
 import ThemeToggle from "./ThemeToggle";
 import { MdNoteAlt } from "react-icons/md";
+import SearchInput from "./SearchInput";
+import Notifications from "./Notifications";
+import UserButton from "./UserButton";
 
 const NavBar = () => {
   return (
-    <nav className="sticky top-0 border-b z-50 bg-white dark:bg-slate-600">
+    <nav className="w-full sticky top-0 border-b z-50 bg-white dark:bg-slate-900">
       <Container>
         <div className="flex justify-between items-center gap-8">
           <div className="flex items-center gap-1 cursor-pointer">
             <MdNoteAlt size={24} />
             <div className="font-bold text-xl">WEBDEV.blog</div>
           </div>
-          <div>Search</div>
+          <SearchInput />
           <div className="flex gap-5 sm:gap-8 items-center">
             <ThemeToggle />
-            <div>Notifications</div>
-            <div>UserMenu</div>
+            <Notifications />
+            <UserButton />
           </div>
         </div>
       </Container>
